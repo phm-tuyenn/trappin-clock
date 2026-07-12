54dfb2f4053cb9ba170e483554b28d851b975a08
phm-tuyenn <phamthanhtuyen2k8@gmail.com>
2026-07-12 09:44:34 +0700

    done basic function. need to add notification if edit time is success or not

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,9 @@
 #define DT_PIN 26
 #define SW_PIN 27
 
+// alarm pin
+#define CLOCK_INTERRUPT_PIN 4
+
 // oled config
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -33,13 +36,17 @@ bool isAlarming = false;
 bool isEditing = false;
 bool isLocking = false;
 
-// alarm
-RTC_DATA_ATTR DateTime sleepTime = DateTime(2026, 7, 7, 8, 0, 0);
-RTC_DATA_ATTR DateTime wakeTime = DateTime(2026, 7, 7, 8, 0, 0);
-DateTime now;
+// time util
+DateTime now, editedNow, editedSleepTime, editedWakeTime;
+TimeSpan editTime(0);
+char editOrder[10][13]   = {"Year", "Month", "Day", "Hour", "Minute", "Second", "Sleep Hour", "Sleep Minute", "Wake Hour", "Wake Minute"};
+unsigned int daysOfMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int editPos = -1;
+bool editSecondOnce = false;
 
 // encoder count
 volatile int counter = 0;
+volatile int direction = 0;
 int lastStateCLK;
 
 // button
@@ -49,6 +56,11 @@ unsigned long systemTime = 0;
 
 #pragma endregion
 
+// alarm
+RTC_DATA_ATTR DateTime sleepTime;
+RTC_DATA_ATTR DateTime wakeTime;
+
+#pragma region function
 // oled display print()
 void print(String text = "", uint8_t size = 1, ALIGN align = LEFT) {
   display.setTextSize(size);
@@ -93,17 +105,95 @@ void drawStatusIcons() {
   }
 }
 
+// draw indicator in edit mode
+void drawEditIndicator() {
+  unsigned int pos[10][2] = {{45, 32}, {21, 32}, {4, 32},
+                             {24, 56}, {60, 56}, {96, 56},
+                             {41, 56}, {78, 56}, {41, 56}, {78, 56}};
+  display.setCursor(pos[editPos][0], pos[editPos][1]);
+  display.setTextSize(1);
+  display.print("^");
+}
+
+// prevent variable from off value limit, if off then roll to other end
+int roll(int currVal, int minVal, int maxVal) {
+  if (currVal > maxVal) currVal = minVal;
+  if (currVal < minVal) currVal = maxVal;
+  return currVal;
+}
+
+// limit the value
+int limit(int currVal, int minVal, int maxVal) {
+  if (currVal > maxVal) currVal = maxVal;
+  if (currVal < minVal) currVal = minVal;
+  return currVal;
+}
+
 // encoder count
-void IRAM_ATTR rotary_encoder() {
+void IRAM_ATTR dial() {
     int currentStateCLK = digitalRead(CLK_PIN);
     if (currentStateCLK != lastStateCLK) {
-        if (digitalRead(DT_PIN) != currentStateCLK) {
-            counter++;
-        } else {
-            counter--;
+      if (digitalRead(DT_PIN) != currentStateCLK) {
+          direction = 1;
+      } else {
+          direction = -1;
+      }
+      counter += direction;
+      Serial.print(counter); Serial.print(" "); Serial.println(direction);
+    
+      if (isEditing) {
+        switch (editPos) {
+          case 0: {// year
+            editTime = editTime + (DateTime(limit(editedNow.year() + direction, 2000, 2135), editedNow.month(), editedNow.day(), editedNow.hour(), editedNow.minute(), editedNow.second()) - editedNow);
+            break;
+          }
+          case 1: {// month
+            editTime = editTime + (DateTime(editedNow.year(), roll(editedNow.month() + direction, 1, 12), editedNow.day(), editedNow.hour(), editedNow.minute(), editedNow.second()) - editedNow);
+            break;
+          }
+          case 2: {// day
+            int month = editedNow.month(), year = editedNow.year();
+            int days = daysOfMonth[month - 1];
+            if ((month == 2) && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) days = 29;
+            editTime = editTime + (DateTime(editedNow.year(), editedNow.month(), roll(editedNow.day() + direction, 1, days), editedNow.hour(), editedNow.minute(), editedNow.second()) - editedNow);
+            break;
+          }
+          case 3: {// hour
+            editTime = editTime + (DateTime(editedNow.year(), editedNow.month(), editedNow.day(), roll(editedNow.hour() + direction, 0, 23), editedNow.minute(), editedNow.second()) - editedNow);
+            break;
+          }
+          case 4: {// minute
+            editTime = editTime + (DateTime(editedNow.year(), editedNow.month(), editedNow.day(), editedNow.hour(), roll(editedNow.minute() + direction, 0, 59), editedNow.second()) - editedNow);
+            break;
+          }
+          case 5: {// second
+            if (!editSecondOnce) {
+              editTime = editTime + (DateTime(editedNow.year(), editedNow.month(), editedNow.day(), editedNow.hour(), editedNow.minute(), 60) - editedNow);
+              editSecondOnce = true;
+            }
+            break;
+          }
+          case 6: {// sleep hour
+            editedSleepTime = DateTime(2000, 1, 1, roll(editedSleepTime.hour() + direction, 0, 23), editedSleepTime.minute(), 0);
+            break;
+          }
+          case 7: {// sleep minute
+            editedSleepTime = DateTime(2000, 1, 1, editedSleepTime.hour(), roll(editedSleepTime.minute() + direction, 0, 59), 0);
+            break;
+          }
+          case 8: {// wake hour
+            editedWakeTime = DateTime(2000, 1, 1, roll(editedWakeTime.hour() + direction, 0, 23), editedWakeTime.minute(), 0);
+            break;
+          }
+          case 9: {// wake minute
+            editedWakeTime = DateTime(2000, 1, 1, editedWakeTime.hour(), roll(editedWakeTime.minute() + direction, 0, 59), 0);
+            break;
+          }
         }
+        editedNow = now + editTime;
+      }
     }
-    lastStateCLK = currentStateCLK;
+  lastStateCLK = currentStateCLK;
 }
 
 // short press handling
@@ -111,15 +201,58 @@ void handleShortPress() {
   if (!isEditing) {
     isAlarming = false;
   } else {
-    // switch to another term to edit
+    if (editPos + 1 > 9) {
+      editPos = 0;
+    } else {
+      editPos++;
+    }
+    if (editPos == 4 || editPos == 6) {
+      editSecondOnce = false;
+    }
+    counter = 0;
+    systemTime = millis();
   }
 }
 
 // long press handling
 void handleLongPress() {
   // switch to edit mode
-  isEditing = !isEditing;
+  if (!isEditing) {
+    isEditing = true;
+    editPos = 0;
+    editedSleepTime = sleepTime;
+    editedWakeTime = wakeTime;
+    counter = 0;
+  }
+  else {
+    isEditing = false;
+    // save change
+    rtc.adjust(editedNow);
+    sleepTime = editedSleepTime;
+    wakeTime = editedWakeTime;
+    rtc.setAlarm1(sleepTime, DS3231_A1_Hour);
+    rtc.setAlarm2(wakeTime, DS3231_A2_Hour);
+    // reset change
+    editTime = TimeSpan(0);
+    editPos = -1;
+    counter = 0;
+  }
+  systemTime = millis();
 }
+
+// handle double click
+void handleDoubleClick() {
+  if (isEditing) {
+    isEditing = false;
+    // reset change
+    editTime = TimeSpan(0);
+    editedSleepTime = sleepTime;
+    editedWakeTime = wakeTime;
+    editPos = -1;
+    counter = 0;
+  }
+}
+#pragma endregion
 
 void setup() {
   Serial.begin(115200);
@@ -137,11 +270,15 @@ void setup() {
     Serial.println("rtc not found");
     delay(2000);
   }
-
+  rtc.disable32K();
+  rtc.writeSqwPinMode(DS3231_OFF);
   if (rtc.lostPower()) {
     Serial.println("reset time because rtc lost power");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
+  pinMode(CLOCK_INTERRUPT_PIN, INPUT_PULLUP);
+  sleepTime = rtc.getAlarm1();
+  wakeTime = rtc.getAlarm2();
   
   // buzzer
   pinMode(2, OUTPUT);
@@ -149,11 +286,12 @@ void setup() {
   // knob encoder
   pinMode(CLK_PIN, INPUT);
   pinMode(DT_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(CLK_PIN), rotary_encoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CLK_PIN), dial, CHANGE);
   
   // button
   button.attachClick(handleShortPress);
   button.attachLongPressStart(handleLongPress);
+  button.attachDoubleClick(handleDoubleClick);
   button.setPressMs(1000);
 
   systemTime = millis();
@@ -161,45 +299,85 @@ void setup() {
 
 void loop() {
   now = rtc.now();
-  // getting each time field in individual variables
-  // adding a leading zero when needed
-  String yearStr = String(now.year(), DEC);
-  String monthStr = (now.month() < 10 ? "0" : "") + String(now.month(), DEC);
-  String dayStr = (now.day() < 10 ? "0" : "") + String(now.day(), DEC);
-  String hourStr = (now.hour() < 10 ? "0" : "") + String(now.hour(), DEC); 
-  String minuteStr = (now.minute() < 10 ? "0" : "") + String(now.minute(), DEC);
-  String secondStr = (now.second() < 10 ? "0" : "") + String(now.second(), DEC);
-  String dayOfWeek = daysOfTheWeek[now.dayOfTheWeek()];
+  editedNow = now + editTime;
 
-  // complete time string
-  String date = dayStr + "-" + monthStr + "-" + yearStr;
-  String time = hourStr + ":" + minuteStr + ":" + secondStr;
-
-  // print time
+  #pragma region display
   display.clearDisplay();
   display.setCursor(0, 6);
+  // bruh put this line in here or time toString not work
+  char dateFormat[] = "DD-MM-YYYY", timeFormat[] = "hh:mm:ss", alarmFormat[] = "hh:mm";
+  if (editPos < 6) {
+    // complete time string
+    String date = editedNow.toString(dateFormat);
+    String time = editedNow.toString(timeFormat);
+    String dayOfWeek = daysOfTheWeek[editedNow.dayOfTheWeek()];
 
-  println(dayOfWeek + "\n");
-  println(date + "\n");
+    // editing status or weekday
+    if (!isEditing) {
+      println(dayOfWeek + "\n");
+    } else {
+      String order = editOrder[editPos];
+      println("Editing: " + order + "\n");
+    }
+    // show date
+    if (!isEditing || (editPos >= 0 && editPos <= 2)) {
+      println(date + "\n"); 
+    } else {
+      println("\n");
+    }
+    // show time
+    if (!isEditing || (editPos >= 3 && editPos <= 5)) {
+      println(time, 2, CENTER);
+    } else {
+      println("\n");
+    }
+  } else {
+    String alarmTime;
+    if (editPos == 6 || editPos == 7) { // sleep
+      alarmTime = editedSleepTime.toString(alarmFormat);
+    } else { // wake
+      alarmTime = editedWakeTime.toString(alarmFormat);
+    }
+    String order = editOrder[editPos];
+    println("Editing: " + order + "\n");
+    println("\n");
+    println(alarmTime, 2, CENTER);
+  }
 
-  println(time, 2, CENTER);
+  if (!isEditing) {
+    // tempurature
+    display.setCursor(0, 6);
+    println(String(rtc.getTemperature(), 1) + " C", 1, RIGHT);
+    display.drawCircle(116, 8, 2, SH110X_WHITE);
+  } 
+  #pragma endregion
 
-  // tempurature
-  display.setCursor(0, 6);
-  println(String(rtc.getTemperature(), 1) + " C", 1, RIGHT);
-  display.drawCircle(116, 8, 2, SH110X_WHITE);
-
-  // alarm toggle
-  if (now.hour() == wakeTime.hour() && now.minute() == wakeTime.minute() && now.second() == 0) {
+  // alarm
+  if (rtc.alarmFired(1)) { // sleep alarm
+    rtc.clearAlarm(1);
+    isLocking = true;
+    systemTime = millis();
+  } else if (rtc.alarmFired(2)) { // wake alarm
+    rtc.clearAlarm(2);
+    isLocking = false;
     isAlarming = true;
   }
-  // create interrupted buzzer effect
-  if (millis() - systemTime <= 200) 
-    digitalWrite(2, isAlarming);
-  else {
-    digitalWrite(2, LOW);
-    systemTime = millis();
-  }
+
+  // buzzer manage
+  if (isAlarming) {
+    // create interrupted buzzer effect
+    if (millis() - systemTime <= 200) {
+      digitalWrite(2, HIGH);
+    } else {
+      digitalWrite(2, LOW);
+      systemTime = millis();
+    }
+  } else {
+    // buzzing when entering locking mode or switch in edit mode
+    // disable buzzer after .1 sec from locking
+    if (millis() - systemTime >= 100) digitalWrite(2, LOW);
+    else digitalWrite(2, HIGH);
+  } 
   
   // set current time using sync-time.py
   if (Serial.available()) {
@@ -208,6 +386,7 @@ void loop() {
     Serial.println("ok");
   } 
 
+  if (isEditing) drawEditIndicator();
   drawStatusIcons();
   display.display();
   button.tick();
--- sync-time.py
+++ sync-time.py
@@ -6,9 +6,20 @@ BAUDRATE = 115200
 
 try:
     with serial.Serial(PORT, BAUDRATE) as ser:
-        message = datetime.now().strftime('%Y %m %d %H %M %S') + "\n"
-        ser.write(message.encode('utf-8'))
+        time.sleep(2)
+        message = (datetime.now()).strftime('%Y %m %d %H %M %S')
+        ser.write(message.encode())
         print(f"Sent: {message.strip()}")
-        
+        while True:
+            # Check if there is data waiting in the buffer
+            if ser.in_waiting > 0:
+                # Read a line from the serial port
+                raw_data = ser.readline()
+                
+                # Decode the bytes into a readable string and remove trailing spacing
+                decoded_string = raw_data.decode('utf-8').rstrip()
+                
+                print(f"Received: {decoded_string}")
+
 except serial.SerialException as e:
     print(f"Error opening or communicating on port {PORT}: {e}")
